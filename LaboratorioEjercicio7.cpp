00ddf57b288b0928b2c0463d96fb73fef109cc0e
FrancoZeg <101823582+FrancoZeg@users.noreply.github.com>
2022-09-05 19:37:56 -0500

    Add files via upload

---
+++ LaboratorioEjercicio7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "* * * * * * * * " << endl;
+  cout << " * * * * * * * *" << endl;
+  cout << "* * * * * * * * " << endl;
+  cout << " * * * * * * * *" << endl;
+  cout << "* * * * * * * * " << endl;
+  cout << " * * * * * * * *" << endl;
+  cout << "* * * * * * * * " << endl;
+  cout << " * * * * * * * *" << endl;
+  cout << "Usando la menor cantidad de statements posible:\n" << endl;
+  cout << "* * * * * * * * \n" << " * * * * * * * *\n" << "* * * * * * * * \n" << " * * * * * * * *\n" << "* * * * * * * * \n" << " * * * * * * * *\n" << "* * * * * * * * \n" << " * * * * * * * *\n" << endl;
+}
